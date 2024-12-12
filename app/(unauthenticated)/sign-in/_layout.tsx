69d29d16b677e0766129041467f3232dbcc19d4b
Ashhar Ali Ahmed <ashhar1111981@gmail.com>
2024-12-12 19:58:45 +0530

    Structure - updated the folder and file structure

---
+++ app/(unauthenticated)/sign-in/_layout.tsx
@@ -0,0 +1,19 @@
+import { Tabs } from 'expo-router';
+import React from 'react';
+import { Platform, Text, View } from 'react-native';
+
+import { HapticTab } from '@/components/HapticTab';
+import { IconSymbol } from '@/components/ui/IconSymbol';
+import TabBarBackground from '@/components/ui/TabBarBackground';
+import { Colors } from '@/constants/Colors';
+import { useColorScheme } from '@/hooks/useColorScheme';
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+<View>
+  <Text>asdasd</Text>
+</View>
+  );
+}

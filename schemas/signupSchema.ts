522cd947d0ff1cb4535d967156f55ecbe92dba52
Ashhar Ali Ahmed <ashhar1111981@gmail.com>
2024-12-30 17:47:22 +0530

    FEAT - OTP varification added

--- schemas/signupSchema.ts
+++ schemas/signupSchema.ts
@@ -1,6 +1,6 @@
 import { z } from "zod"
 
-const signupSchema = z.object({
+export const signupSchema = z.object({
   email: z.string().email("Invalid email address"),
   firstName: z.string().min(1, "First name is required"),
   lastName: z.string().min(1, "Last name is required"),
@@ -15,4 +15,11 @@ const signupSchema = z.object({
   //   .nonempty("Password is required")
   //   .min(6, "Password must be at least 6 characters long")
 })
-export default signupSchema
+
+export const otpSchema = z.object({
+  OTP: z
+    .string()
+    .regex(/^\d+$/, "OTP must contain only numbers.")
+    .length(6, "OTP must be exactly 6 digits."),
+  userId: z.string().nonempty("User ID is required."),
+})

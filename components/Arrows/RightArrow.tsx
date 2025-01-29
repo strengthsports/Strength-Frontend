70b1bf4a56b9f4f3045004243eeb208f2519bd83
Anirban Dutta <meanirbandutta22@gmail.com>
2025-01-29 13:44:45 +0530

    feat - new component right arrow added

---
+++ components/Arrows/RightArrow.tsx
@@ -0,0 +1,12 @@
+import { AntDesign } from "@expo/vector-icons";
+
+export default function RightArrow() {
+  return (
+    <AntDesign
+      name="right"
+      style={{ paddingRight: 5 }}
+      size={17}
+      color="grey"
+    />
+  );
+}

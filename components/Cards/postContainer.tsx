9ab396df11ac49fd711b7247575e96355d9f2e99
Ashhar Ali Ahmed <ashhar1111981@gmail.com>
2025-01-31 18:55:05 +0530

    REFACTOR - optimised feed page

--- components/Cards/postContainer.tsx
+++ components/Cards/postContainer.tsx
@@ -142,13 +142,13 @@ const PostContainer = ({ item }: { item: Post }) => {
       backgroundColor: "grey",
       width: 4,
       height: 4,
-      marginHorizontal: 20,
+      // marginHorizontal: 20,
     },
     activeDotStyle: {
       backgroundColor: "white",
       width: 5,
       height: 5,
-      marginLeft: 20,
+      // marginLeft: 20,
       zIndex: 30,
     },
   };

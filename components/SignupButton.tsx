1d2971fd9fb5bafafa5b7c8b410284bb7535a7e1
Anirban Dutta <meanirbandutta22@gmail.com>
2025-02-18 00:57:13 +0530

    refactor - new prop added

--- components/SignupButton.tsx
+++ components/SignupButton.tsx
@@ -1,31 +1,57 @@
-import { StyleSheet, Pressable, GestureResponderEvent, Text, TouchableOpacity, Platform } from 'react-native';
-import React from 'react';
+import {
+  StyleSheet,
+  Pressable,
+  GestureResponderEvent,
+  Text,
+  TouchableOpacity,
+  Platform,
+} from "react-native";
+import React from "react";
 
 // Define the prop types
 interface SignupButtonProps {
   onPress: (event: GestureResponderEvent) => void; // onPress handler
   children: React.ReactNode; // Button content
+  disabled: boolean;
 }
 
-const SignupButton: React.FC<SignupButtonProps> = ({ onPress, children }) => {
-  return (
-    Platform.OS === 'android' ? (
-      <Pressable
-        onPress={onPress}
-        android_ripple={{ color: '#121212' }} // Ripple effect for Android
-        style={{ width: 335, height: 42, backgroundColor: '#12956B', justifyContent: 'center', alignItems: 'center', borderRadius: 40 }}
-      >
-        {children}
-      </Pressable>
-    ) : (
-      <TouchableOpacity
-        onPress={onPress}
-        activeOpacity={0.5}
-        style={{ width: 335, height: 42, backgroundColor: '#12956B', justifyContent: 'center', alignItems: 'center', borderRadius: 40 }}
-      >
-        {children}
-      </TouchableOpacity>
-    )
+const SignupButton: React.FC<SignupButtonProps> = ({
+  onPress,
+  children,
+  disabled,
+}) => {
+  return Platform.OS === "android" ? (
+    <Pressable
+      onPress={onPress}
+      android_ripple={{ color: "#121212" }} // Ripple effect for Android
+      style={{
+        width: 335,
+        height: 42,
+        backgroundColor: "#12956B",
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 40,
+      }}
+      disabled={disabled}
+    >
+      {children}
+    </Pressable>
+  ) : (
+    <TouchableOpacity
+      onPress={onPress}
+      activeOpacity={0.5}
+      style={{
+        width: 335,
+        height: 42,
+        backgroundColor: "#12956B",
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 40,
+      }}
+      disabled={disabled}
+    >
+      {children}
+    </TouchableOpacity>
   );
 };
 

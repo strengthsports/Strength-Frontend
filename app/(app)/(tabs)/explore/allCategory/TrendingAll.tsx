fed0518d2664c7e20dcd5a746aef22b9b4a8c7c5
Ashhar Ali Ahmed <ashhar1111981@gmail.com>
2025-02-22 19:23:24 +0530

    FIX - Reduced multiple API calls in cricket

--- app/(app)/(tabs)/explore/allCategory/TrendingAll.tsx
+++ app/(app)/(tabs)/explore/allCategory/TrendingAll.tsx
@@ -8,12 +8,11 @@ import { ExploreImageBanner, hashtagData } from "~/constants/hardCodedFiles";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import Hashtag from "~/components/explorePage/hashtag";
 import MatchCard from "~/components/explorePage/cricketMatchCard";
-import { useGetLiveCricketMatchesQuery, useGetNextCricketMatchQuery } from "~/reduxStore/api/explore/cricketApi";
+// import { useGetLiveCricketMatchesQuery, useGetNextCricketMatchQuery } from "~/reduxStore/api/explore/cricketApi";
 import NextMatchCard from "~/components/explorePage/cricketNextMatchCard";
+import { useGetCricketMatchesQuery } from "~/reduxStore/api/explore/cricketApi";
 
 const TrendingAll = () => {
-  const { data: liveCricketMatches, isFetching: isFetchingLiveCricket, refetch: refetchLiveCricket } = useGetLiveCricketMatchesQuery({});
-  const { data: nextCricketMatches, isFetching: isFetchingNextCricket, refetch: refetchNextCricket } = useGetNextCricketMatchQuery({});
 
   const renderSwiper = () => (
     <Swiper
@@ -65,6 +64,8 @@ const TrendingAll = () => {
     </View>
   );
 
+  const { data, isFetching, refetch: refetchLiveCricket } = useGetCricketMatchesQuery({});
+  const  { liveMatches: liveCricketMatches, nextMatch: nextCricketMatches } = data || {};
   const renderCricketLiveMatches = () => {
     return (
       <View className="mt-7">
@@ -83,7 +84,7 @@ const TrendingAll = () => {
           contentContainerStyle={{ paddingHorizontal: 20 }}
           renderItem={({ item }) => (
             <View className="h-56 w-96 bg-transparent rounded-2xl mr-5 border border-neutral-600 ">
-              {isFetchingLiveCricket ? (
+              {isFetching ? (
                 <View className="h-full flex justify-center self-center items-center">
                   <ActivityIndicator size="large" color={Colors.themeColor} />
                 </View>
@@ -104,17 +105,55 @@ const TrendingAll = () => {
     );
   };
 
+  // const renderFootballLiveMatches = () => {
+  //   return (
+  //     <View className="mt-7">
+  //       <View className="flex-row items-center justify-between pl-7 pr-10 mb-4">
+  //         <View className="flex-row items-center ">
+  //           <TextScallingFalse className="text-white text-6xl font-bold">Matches</TextScallingFalse>
+  //           <MaterialCommunityIcons name="chevron-double-right" size={22} color="white" className="-mb-1" />
+  //         </View>
+  //         <MaterialCommunityIcons name="reload" size={22} color="grey" className="-mb-1" onPress={refetchLiveCricket} />
+  //       </View>
+  //       <FlatList
+  //         data={liveCricketMatches}
+  //         keyExtractor={(item) => item.id.toString()}
+  //         horizontal
+  //         showsHorizontalScrollIndicator={false}
+  //         contentContainerStyle={{ paddingHorizontal: 20 }}
+  //         renderItem={({ item }) => (
+  //           <View className="h-56 w-96 bg-transparent rounded-2xl mr-5 border border-neutral-600 ">
+  //             {isFetching ? (
+  //               <View className="h-full flex justify-center self-center items-center">
+  //                 <ActivityIndicator size="large" color={Colors.themeColor} />
+  //               </View>
+  //             ) : (
+  //               <MatchCard match={item} isLive={true} />
+  //             )}
+  //           </View>
+  //         )}
+  //         ListEmptyComponent={
+  //           <View className="w-screen justify-center mt-10">
+  //             <TextScallingFalse className="text-white self-center text-center pr-7">
+  //               No live matches available
+  //             </TextScallingFalse>
+  //           </View>
+  //         }
+  //       />
+  //     </View>
+  //   );
+  // };
+
   const renderCricketNextMatches = () => {
     return (
       <View className="mt-7">
         <View className="flex-row items-center justify-between pl-7 pr-10 mb-4">
           <TextScallingFalse className="text-white text-6xl font-bold">Don't Miss</TextScallingFalse>
-          <MaterialCommunityIcons name="reload" size={22} color="grey" className="-mb-1" onPress={refetchNextCricket} />
         </View>
         {nextCricketMatches ? (
           <View className="px-6">
             <View className="h-56 w-full rounded-2xl bg-neutral-900 mr-5 border border-neutral-600 ">
-              {isFetchingNextCricket ? (
+              {isFetching ? (
                 <View className="h-full flex justify-center self-center items-center">
                   <ActivityIndicator size="large" color={Colors.themeColor} />
                 </View>
@@ -134,11 +173,13 @@ const TrendingAll = () => {
     );
   };
 
+
   const sections = [
     { type: "swiper", content: renderSwiper() },
     { type: "divider", content: <View className="h-[0.6px] bg-neutral-600" /> },
     { type: "hashtags", content: renderHashtags() },
     { type: "CricketLiveMatches", content: renderCricketLiveMatches() },
+    // { type: "FootballLiveMatches", content: renderFootballLiveMatches() },
     { type: "CricketNextMatches", content: renderCricketNextMatches() },
   ];
 

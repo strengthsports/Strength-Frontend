7112beb6410add2c60cda5b8c1e45c228cfab7a1
Anirban Dutta <meanirbandutta22@gmail.com>
2025-03-08 17:36:20 +0530

    fix - established on date

--- reduxStore/slices/team/teamSlice.ts
+++ reduxStore/slices/team/teamSlice.ts
@@ -36,7 +36,7 @@ type TeamPayload = {
 function convertToDate(dateString) {
   // Validate input format (M/YYYY or MM/YYYY)
   if (!dateString || !/^\d{1,2}\/\d{4}$/.test(dateString)) {
-      throw new Error("Invalid date format. Use 'M/YYYY' or 'MM/YYYY'.");
+    throw new Error("Invalid date format. Use 'M/YYYY' or 'MM/YYYY'.");
   }
 
   // Extract month and year
@@ -47,13 +47,11 @@ function convertToDate(dateString) {
 
   // Validate the date
   if (isNaN(dateObject.getTime())) {
-      throw new Error("Invalid date conversion.");
+    throw new Error("Invalid date conversion.");
   }
 
-  return dateObject; // MongoDB will store this as ISODate
+  return dateObject.toISOString().split("T")[0]; // MongoDB will store this as ISODate
 }
-
-
 
 export const createTeam = createAsyncThunk<
   Team,
@@ -63,24 +61,21 @@ export const createTeam = createAsyncThunk<
   try {
     const token = await getToken("accessToken");
     console.log("Team Data:", teamData);
-    
+
     const formData = new FormData();
     const datee = convertToDate(teamData.establishedOn);
     console.log("datee", datee);
     formData.append("name", teamData.name);
     formData.append("sport", "6771941c77a19c8141f2f1b7");
-    
+
     formData.append("establishedOn", datee);
     formData.append("gender", teamData.gender);
     formData.append("description", teamData.description);
     // formData.append("location",null);
     formData.append("assets", teamData.logo);
-    
-  
-    
 
     console.log("Sending FormData:", formData);
-    
+
     const response = await fetch(
       `${process.env.EXPO_PUBLIC_BASE_URL}/api/v1/team`,
       {
@@ -105,10 +100,6 @@ export const createTeam = createAsyncThunk<
     return rejectWithValue(error.message || "Network error!");
   }
 });
-
-
-
-
 
 //edit team
 

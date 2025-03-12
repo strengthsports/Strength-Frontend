137e717112bc9ea6d48d89e26ff0b4be5cac9c82
Anirban Dutta <meanirbandutta22@gmail.com>
2025-03-13 01:46:40 +0530

    feat - post details page

---
+++ components/ui/SwiperImage.tsx
@@ -0,0 +1,40 @@
+import { View, Text } from "react-native";
+import React, { memo, useRef } from "react";
+import { TouchableOpacity } from "react-native";
+import { Image } from "react-native";
+import { useRouter } from "expo-router";
+
+export interface SwiperImageProps {
+  uri?: string;
+  details?: any;
+  onDoubleTap?: () => void;
+  className?: string;
+}
+
+const SwiperImage = memo<SwiperImageProps>(
+  ({ uri, details, onDoubleTap, className }) => {
+    const router = useRouter();
+    let lastTap = useRef(0).current;
+    // console.log("Details : ", details);
+
+    return (
+      <TouchableOpacity
+        activeOpacity={0.5}
+        onPress={() =>
+          router.push({
+            pathname: "/(app)/(modal)/post/1",
+            params: { image: uri, details: JSON.stringify(details) },
+          })
+        }
+      >
+        <Image
+          className={`w-full h-full object-cover ${className}`}
+          source={{ uri }}
+          resizeMode="cover"
+        />
+      </TouchableOpacity>
+    );
+  }
+);
+
+export default SwiperImage;

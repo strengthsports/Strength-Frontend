38262074f7c687f3cc8e40eb0563f63bcd201de4
Amit Saha <asaha.bwn2000@gmail.com>
2025-04-14 00:04:57 +0530

    profile page ui fix

--- app/(app)/(profile)/profile/[userId]/index.tsx
+++ app/(app)/(profile)/profile/[userId]/index.tsx
@@ -8,7 +8,7 @@ import {
   Text,
   useWindowDimensions,
 } from "react-native";
-import React, { useContext, useEffect, useMemo, useState } from "react";
+import React, { useContext, useEffect, useMemo, useState, useRef } from "react";
 import TextScallingFalse from "~/components/CentralText";
 import { responsiveFontSize } from "react-native-responsive-dimensions";
 import { Tabs, TabsContent, TabsList } from "~/components/ui/tabs";
@@ -64,18 +64,32 @@ const Overview = () => {
 
   const [isExpanded, setIsExpanded] = useState(false);
 
+  const { profileData, isLoading, error } = useContext(ProfileContext);
+  console.log("User data on Overview page : ", profileData);
+
+  const maxAboutLength = 140;
+  const aboutText = profileData?.about || '';
+  const needsTruncation = aboutText.length > maxAboutLength;
+  const truncatedText = needsTruncation ? `${aboutText.substring(0, maxAboutLength).trim()}...` : aboutText;
+ 
   const handleToggle = () => {
     setIsExpanded(!isExpanded);
   };
-
-  const { profileData, isLoading, error } = useContext(ProfileContext);
-  console.log("User data on Overview page : ", profileData);
 
   const validSports =
     profileData?.selectedSports?.filter((s: any) => s.sport) || [];
   const [activeSubSection, setActiveSubSection] = useState(
     validSports[0]?.sport.name || null
   );
+
+  const scrollViewRef = useRef<ScrollView>(null);
+  const handleScrollToStart = () => {
+    scrollViewRef.current?.scrollTo({
+      x: 0,
+      y: 0,
+      animated: true,
+    });
+  }
 
   if (error) {
     return (
@@ -110,6 +124,7 @@ const Overview = () => {
             showsHorizontalScrollIndicator={false}
             contentContainerStyle={{ paddingStart: 15 * scaleFactor }}
             className="mt-2"
+            ref={scrollViewRef}
           >
             <TabsList className="flex-row gap-x-2 w-[100%]">
               {validSports.map((sport: any) => (
@@ -148,9 +163,9 @@ const Overview = () => {
                 </TouchableOpacity>
               ))}
               <TouchableOpacity
-                className="border-[0.5px] border-[#686868] rounded-lg flex items-center justify-center"
+                className="border-[0.5px] border-[#686868] rounded-lg flex items-center justify-center mr-40"
                 style={{ width: 36 * scaleFactor, height: 36 * scaleFactor }}
-                onPress={() => router.push("/(app)/(profile)/edit-overview")}
+                onPress={handleScrollToStart}
               >
                 <Feather
                   name="chevron-right"
@@ -168,12 +183,12 @@ const Overview = () => {
               <View className="w-full md:max-w-[600px] mx-auto flex-1 items-center p-2">
                 {sport.details && (
                   <View className="bg-[#161616] w-[96%] px-5 py-4 rounded-[15px]">
-                    <View className="flex-row justify-start flex-wrap gap-y-4">
+                    <View className="flex-row justify-start flex-wrap gap-8 gap-y-4">
                       {Object.entries(sport.details).map(
                         ([key, value], idx) => (
                           <View
                             key={idx}
-                            className={`${idx < 3 ? "basis-[33%]" : "w-full"}`}
+                            // className={`${idx < 3 ? "basis-[33%]" : "w-full"}`}
                           >
                             <Text
                               className="text-white font-bold"
@@ -277,16 +292,17 @@ const Overview = () => {
                 style={{
                   fontSize: responsiveFontSize(1.6),
                 }}
-                numberOfLines={isExpanded ? undefined : 2}
               >
-                {profileData?.about}
+                {isExpanded ? aboutText : truncatedText}
+                 {needsTruncation && (
+                  <TextScallingFalse
+                    onPress={handleToggle}
+                    className="text-[#808080] font-light text-lg"
+                  >
+                    {isExpanded ? '  see less' : ' see more'}
+                  </TextScallingFalse>
+                )}
               </TextScallingFalse>
-              <TouchableOpacity onPress={handleToggle}>
-                <TextScallingFalse style={styles.seeMore}>
-                  {profileData?.about.length > 140 &&
-                    (isExpanded ? "see less" : "see more")}
-                </TextScallingFalse>
-              </TouchableOpacity>
             </View>
           </View>
         </View>
--- app/(app)/(profile)/profile/[userId]/media.tsx
+++ app/(app)/(profile)/profile/[userId]/media.tsx
@@ -1,14 +1,94 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  FlatList,
+  Dimensions,
+  ActivityIndicator,
+} from "react-native";
+import React, { memo, useContext, useEffect, useMemo } from "react";
+import { useSelector } from "react-redux";
+import TextScallingFalse from "~/components/CentralText";
+import { useLocalSearchParams } from "expo-router";
+import { useLazyGetSpecificUserPostQuery } from "~/reduxStore/api/profile/profileApi.post";
+import { ProfileContext } from "./_layout";
 
-const media = () => {
+const Media = () => {
+  const params = useLocalSearchParams();
+     
+  const fetchedUserId = useMemo(() => {
+    return params.userId
+      ? JSON.parse(decodeURIComponent(params?.userId as string))
+      : null;
+  }, [params.userId]);
+ 
+  const [getUserSpecificPost, { data: posts }] = useLazyGetSpecificUserPostQuery();
+       
+  useEffect(() => {
+    getUserSpecificPost({
+      postedBy: fetchedUserId?.id,
+      postedByType: fetchedUserId?.type,
+      limit: 10,
+      skip: 0,
+    });
+  }, []);
+ 
+  const { profileData, isLoading, error } = useContext(ProfileContext);
+
+  const memoizedEmptyComponent = memo(() => (
+    <Text className="text-white text-center p-4">No images available</Text>
+  ));
+
+  // Extract URLs from posts
+  const imageUrls = posts
+    ?.flatMap((post: any) => post.assets) // Flatten assets from all posts
+    ?.map((asset: any) => asset.url) // Extract URLs
+    ?.filter((url: any) => url); // Remove empty values
+
+  if (isLoading)
+    return (
+      <View className="flex justify-center items-center">
+        <ActivityIndicator color="#12956B" size={22} />
+      </View>
+    );
+  if (error)
+    return (
+      <View className="flex justify-center items-center">
+        <TextScallingFalse className="text-red-500">
+          {" "}
+          Error loading posts
+        </TextScallingFalse>
+      </View>
+    );
+
   return (
-    <View>
-      <Text>Media</Text>
+    <View className="flex-1 mt-[2px]">
+      <FlatList
+        data={imageUrls}
+        keyExtractor={(item, index) => `${item}-${index}`}
+        numColumns={3}
+        renderItem={({ item }) => (
+          <Image
+            source={{ uri: item }}
+            resizeMode="cover"
+            style={styles.image}
+          />
+        )}
+        ListEmptyComponent={memoizedEmptyComponent}
+      />
     </View>
   );
 };
 
-export default media;
+export default Media;
 
-const styles = StyleSheet.create({});
+const { width } = Dimensions.get("window");
+
+const styles = StyleSheet.create({
+  image: {
+    width: width / 3 - 4,
+    height: width / 3 - 4,
+    margin: 2,
+  },
+});

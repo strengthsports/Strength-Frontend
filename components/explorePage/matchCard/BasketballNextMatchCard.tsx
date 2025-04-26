065f6ce7006851cba06ea76b06b9a6cfa0988303
pranav-prasad16 <prasadpranav736@gmail.com>
2025-04-26 13:19:45 +0530

    football and basketball live matches

---
+++ components/explorePage/matchCard/BasketballNextMatchCard.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const BasketballNextMatchCard = () => {
+  return (
+    <View>
+      <Text>BasketballNextMatchCard</Text>
+    </View>
+  );
+};
+
+export default BasketballNextMatchCard;
+
+const styles = StyleSheet.create({});

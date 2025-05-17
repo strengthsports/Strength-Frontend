c1d3b27dc4564c40a95f7739607f3b9aa02b1223
pranav-prasad16 <prasadpranav736@gmail.com>
2025-05-17 16:07:28 +0530

    sports data api backend connected

--- reduxStore/api/explore/cricketApi.ts
+++ reduxStore/api/explore/cricketApi.ts
@@ -1,148 +1,63 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-
-const API_BASE_URL = process.env.EXPO_PUBLIC_CRICKET_API_BASE_URL;
-const API_KEY = process.env.EXPO_PUBLIC_CRICKET_API;
-
-if (!API_KEY || !API_BASE_URL) {
-  throw new Error("Missing API Key or BASE URL in environment variables");
-}
+import { getToken } from "~/utils/secureStore";
 
 export const cricketApi = createApi({
   reducerPath: "cricketApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: API_BASE_URL,
-    prepareHeaders: (headers) => {
-      headers.set("x-rapidapi-key", API_KEY);
-      headers.set("x-rapidapi-host", "cricbuzz-cricket.p.rapidapi.com");
+    baseUrl: `${process.env.EXPO_PUBLIC_BASE_URL}/api/v1/sportsDataApi/cricket`,
+    prepareHeaders: async (headers) => {
+      const token = await getToken("accessToken");
+      if (token) {
+        headers.set("Authorization", `Bearer ${token}`);
+      }
+      headers.set("Content-Type", "application/json");
       return headers;
     },
   }),
   endpoints: (builder) => ({
     getCricketLiveMatches: builder.query({
-      query: () => `/matches/v1/live`,
+      query: () => `/live`,
       transformResponse: (response: any) => {
-        const typeMatches = Array.isArray(response.typeMatches)
-          ? response.typeMatches
+        const liveMatches = Array.isArray(response.data.liveMatches)
+          ? response.data.liveMatches
           : [];
 
-        const allMatches: any[] = [];
-
-        // Extract live matches
-        typeMatches.forEach((typeMatch: any) => {
-          const seriesMatches = typeMatch?.seriesMatches || [];
-
-          seriesMatches.forEach((seriesMatch: any) => {
-            const matches = seriesMatch?.seriesAdWrapper?.matches;
-            if (Array.isArray(matches)) {
-              allMatches.push(...matches); // flatten and extract matches
-            }
-          });
-        });
-
-        // Extra checking for live match
-        const liveMatches = allMatches.filter((match: any) =>
-          ["In Progress", "Stumps"].includes(match?.matchInfo?.state)
-        );
-
+        // console.log("Live Cricket Matches :", liveMatches);
         return { liveMatches };
       },
     }),
     getCricketNextMatches: builder.query({
-      query: () => `/matches/v1/upcoming`,
+      query: () => `/next`,
       transformResponse: (response: any) => {
-        if (!response || !Array.isArray(response.typeMatches))
-          return { nextMatches: [] };
-
-        const typeMatches = Array.isArray(response.typeMatches)
-          ? response.typeMatches
+        const nextMatches = Array.isArray(response.data.nextMatches)
+          ? response.data.nextMatches
           : [];
 
-        const allMatches: any[] = [];
-
-        // Extract next matches
-        typeMatches.forEach((typeMatch: any) => {
-          const seriesMatches = typeMatch?.seriesMatches || [];
-
-          seriesMatches.forEach((seriesMatch: any) => {
-            const seriesAdWrapper = seriesMatch?.seriesAdWrapper;
-            const matches = seriesAdWrapper?.matches;
-
-            // Extra checking for next match
-            if (Array.isArray(matches)) {
-              const previewMatches = matches.filter(
-                (match: any) => match?.matchInfo?.state === "Preview"
-              );
-
-              if (previewMatches.length > 0) {
-                allMatches.push({
-                  ...seriesAdWrapper,
-                  matches: previewMatches,
-                });
-              }
-            }
-          });
-        });
-
-        return { nextMatches: allMatches };
+        // console.log("Next Cricket Matches :", nextMatches);
+        return { nextMatches };
       },
     }),
     getCricketNextMatchesBySeries: builder.query({
-      query: (seriesId) => `/series/v1/${seriesId}`,
+      query: () => `/next/series`,
       transformResponse: (response: any) => {
-        const matchDetails = Array.isArray(response.matchDetails)
-          ? response.matchDetails
+        const seriesMatches = Array.isArray(response.data.seriesMatches)
+          ? response.data.seriesMatches
           : [];
 
-        const allMatches: any[] = [];
-
-        // Extract matches of same series
-        matchDetails.forEach((matchDetail: any) => {
-          const matches = matchDetail?.matchDetailsMap?.match || [];
-
-          if (Array.isArray(matches)) {
-            allMatches.push(...matches); // flatten and extract matches
-          }
-        });
-
-        const filteredMatches = allMatches.filter((match) =>
-          ["Upcoming", "Abandon"].includes(match?.matchInfo?.state)
-        );
-
-        // console.log("Filtered matches", filteredMatches);
+        // console.log("Series Cricket Matches :", seriesMatches);
         return {
-          seriesMatches: filteredMatches,
+          seriesMatches,
         };
       },
     }),
     getCricketRecentMatches: builder.query({
-      query: () => `/matches/v1/recent`,
+      query: () => `/recent`,
       transformResponse: (response: any) => {
-        const typeMatches = Array.isArray(response.typeMatches)
-          ? response.typeMatches
+        const recentMatches = Array.isArray(response.data.recentMatches)
+          ? response.data.recentMatches
           : [];
 
-        // Extract recent matches
-        const allMatches: any[] = [];
-
-        // Extract recent matches
-        typeMatches.forEach((typeMatch: any) => {
-          const seriesMatches = typeMatch?.seriesMatches || [];
-
-          seriesMatches.forEach((seriesMatch: any) => {
-            const matches = seriesMatch?.seriesAdWrapper?.matches;
-            if (Array.isArray(matches)) {
-              allMatches.push(...matches); // flatten and extract matches
-            }
-          });
-        });
-
-        // Extra checking for recent match
-        const recentMatches = allMatches.filter(
-          (match: any) =>
-            match?.matchInfo?.state === "Complete" &&
-            match?.matchInfo?.stateTitle !== "Abandon"
-        );
-
+        // console.log("Recent Cricket Matches :", recentMatches);
         return { recentMatches };
       },
     }),
